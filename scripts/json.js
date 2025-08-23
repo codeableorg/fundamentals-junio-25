0b2bb21fb1ebfcc3c3df0c1b6479e37b01feb45d
Andre Tavara <andretj_92@live.com>
2025-08-22 21:05:10 -0500

    Agrega la clase del viernes - semana 12

---
+++ scripts/json.js
@@ -0,0 +1,14 @@
+// const escuela = {
+//   nombre: "Codeable",
+//   fundacion: 2019,
+// };
+
+// JSON: JavaScript Object Notation
+// 1. Dar el formato correcto a la data
+// const escuelaEnFormatoJSON = '{"nombre":"Codeable","fundacion":2019}';
+// const json = JSON.stringify(escuela);
+
+// 2. Escribir la informacion en el archivo .json
+
+const jsonData = '["manzana", "pera", "fresa"]';
+const frutas = JSON.parse(jsonData);
--- scripts/promises.js
+++ scripts/promises.js
@@ -1,17 +1,19 @@
-// const promesa = new Promise((resolve, reject) => {
-//   const hasConnectionError = Math.random() > 0.9;
-//   setTimeout(() => {
-//     if (hasConnectionError) {
-//       reject("Disculpas");
-//     } else {
-//       resolve("PS5");
-//     }
-//   }, 2000);
-// });
+/*
+const promesa = new Promise((resolve, reject) => {
+  const hasConnectionError = Math.random() > 0.9;
+  setTimeout(() => {
+    if (hasConnectionError) {
+      reject("Disculpas");
+    } else {
+      resolve("PS5");
+    }
+  }, 2000);
+});
 
-// promesa
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error.message));
+promesa
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error.message));
+*/
 
 const users = [
   {
@@ -28,32 +30,75 @@ const users = [
   },
 ];
 
+const githubPublicRepos = [
+  { id: 1, owner: "pepe-20", name: "hello-world" },
+  { id: 2, owner: "pepe-20", name: "say-hi" },
+  { id: 3, owner: "anita-dev", name: "first-repo" },
+  { id: 4, owner: "anita-dev", name: "codeable" },
+  { id: 5, owner: "anita-dev", name: "example" },
+];
+
+// Funcion que se ejecuta cuando la promesa del User se resuelve
+// Parametro user es un objeto { email, password, name, githubUsername }
+// Valor de retorno es un objeto Promise proporciona por la ejecucion de la funcion getPublicRepost
 function onUserSuccess(user) {
   console.log("¡Bienvenid@ " + user.name + "!");
+
+  return getPublicRepost(user.githubUsername);
 }
 
+// Funcion que se ejecuta cuando la promesa del User se rechaza
+// Parametro error es un objeto de tipo Error
 function onUserError(error) {
   console.log(error.message);
 }
 
+// Funcion que se ejecuta cuando la promesa reposPromise se resuelve
+// Parametro repos es un objeto array con los repositorios de 1 usuario
+function onReposSuccess(repos) {
+  repos.forEach((repo) => console.log(repo.name));
+}
+
+// Funcion que se ejecuta cuando la promesa reposPromise se rechaza
+// Parametro error es un objeto de tipo Error
+function onReposError(error) {
+  console.log(error.message);
+}
+
+// Funciones Asincronas: ambas retornan de manera inmediata un objeto Promise
 function getUser(email, password) {
-  // retornamos una promesa...
   return new Promise((resolve, reject) => {
-    // que después de 2 segundos...
     setTimeout(() => {
       const user = users.find((u) => u.email === email);
       if (!user || user.password !== password) {
         const error = new Error("Credenciales invalidas");
-        // se rechaza si las credenciales son invalidas
         reject(error);
       }
 
-      // se cumple pasando la información del usuario
       resolve(user);
     }, 2000);
   });
 }
 
-const userPromise = getUser("pepe@mail.com", "supersecret");
+function getPublicRepost(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const hasConnectionError = Math.random() > 0.9;
+      if (hasConnectionError) {
+        const error = new Error("Error de conexión");
+        reject(error);
+      } else {
+        const repos = githubPublicRepos.filter(
+          (repo) => repo.owner === username
+        );
 
-userPromise.then(onUserSuccess).catch(onUserError);
+        resolve(repos);
+      }
+    }, 2000);
+  });
+}
+
+// userPromise es un objeto Promise en estado "pendiente"
+const userPromise = getUser("pepe@mail.com", "supersecret");
+// la ejecucion del callback onUserSuccess retorna un nuevo objeto Promise manejado por el segundo then
+userPromise.then(onUserSuccess).then(onReposSuccess).catch(onUserError);
