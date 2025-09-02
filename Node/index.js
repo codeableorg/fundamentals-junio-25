1d5466fbacf3605da060d3ce2e57892200db5f83
Andre Tavara <andretj_92@live.com>
2025-09-01 21:15:06 -0500

    Agrega la clase del lunes - semana 14

--- Node/index.js
+++ Node/index.js
@@ -21,12 +21,38 @@ function requestListener(request, response) {
   console.log(request.url);
 
   if (request.url === "/") {
-    response.writeHead(200);
-    response.write("Bienvenido a Notas");
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(`<!DOCTYPE html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="UTF-8" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                        <title>Notes | Inicio</title>
+                      </head>
+                      <body>
+                        <h1>Bienvenido a Notas!</h1>
+                        <a href="/notes">Ver mis notas</a>
+                      </body>
+                    </html>`);
     response.end();
   } else if (request.url === "/notes") {
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.write(JSON.stringify(notes));
+    const liNotes = notes.map(
+      (note) => `<li><a href="/notes/${note.id}">${note.content}</a></li>`
+    );
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(`<!DOCTYPE html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="UTF-8" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                        <title>Notes</title>
+                      </head>
+                      <body>
+                        <h1>Notas</h1>
+                        <ul>${liNotes.join("")}</ul>
+                        <a href="/new-note">Crear nueva nota</a>
+                      </body>
+                    </html>`);
     response.end();
   } else if (validateUrl(request.url)) {
     const segments = request.url.split("/");
@@ -35,19 +61,67 @@ function requestListener(request, response) {
     const note = notes.find((note) => note.id === noteId);
 
     if (note) {
-      response.writeHead(200, { "Content-Type": "application/json" });
-      response.write(JSON.stringify(note));
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(`<!DOCTYPE html>
+                      <html lang="en">
+                        <head>
+                          <meta charset="UTF-8" />
+                          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                          <title>Note</title>
+                        </head>
+                        <body>
+                          <h1>Detalle de la nota:</h1>
+                          <div>
+                            <p>ID: ${note.id}</p>
+                            <p>Contenido: ${note.content}</p>
+                          </div>
+                          <br>
+                          <a href="/notes">Volver a todas las notas</a>
+                        </body>
+                      </html>`);
       response.end();
     } else {
-      response.writeHead(400, { "Content-Type": "application/json" });
-      response.write(JSON.stringify({ error: "Not found" }));
+      response.writeHead(400, { "Content-Type": "text/html" });
+      response.write(`<!DOCTYPE html>
+                      <html lang="en">
+                        <head>
+                          <meta charset="UTF-8" />
+                          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                          <title>Error</title>
+                        </head>
+                        <body>
+                          <h1>404</h1>
+                          <p>Nota no encontrada</p>
+                        </body>
+                      </html>`);
       response.end();
     }
+  } else if (request.url === "/new-note") {
+    // formulario
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(`<!DOCTYPE html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="UTF-8" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                        <title>Notes | Inicio</title>
+                      </head>
+                      <body>
+                        <h1>Nueva Nota</h1>
+                        <form>
+                          <div>
+                            <label for="new-note">Contenido:</label>
+                            <input id="new-note" type="text" name="new-note" />
+                            <button>Enviar</button>
+                          </div>
+                        </form>
+                      </body>
+                    </html>`);
+    response.end();
   }
 }
 
 const server = http.createServer(requestListener);
-server.listen(5500);
-
-// response.writeHead(200, { "Content-Type": "text/plain" } );
-// response.writeHead(200, { "Content-Type": "text/html" } );
+server.listen(5500, () => {
+  console.log("El servidor ya esta escuchando!");
+});
